6dedf2c89db5dc035093d01f0951215062a4c25c
JNoor <108423527+Noorjannat@users.noreply.github.com>
2024-06-29 21:32:06 +0600

    frontend set up completed

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="{{ asset('frontend/css/venobox.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/jquery.exzoom.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('frontend/css/toastr.min.css')}}">
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
@@ -109,9 +109,23 @@
     <!-- ex zoom js -->
     <script src="{{ asset('frontend/js/jquery.exzoom.js') }}"></script>
 
-    <!--main/custom js-->
-    <script src="{{ asset('frontend/js/main.js') }}"></script>
 
+    <script src="{{ asset('frontend/js/main.js') }}"></script>
+        <!--main/custom js-->
+
+    <script src="{{ asset('frontend/js/toastr.min.js')}}"></script>
+    <!--show dynamic validation message -->
+    <script>
+        toastr.options.progressBar = true;
+
+        @if($errors->any())
+          @foreach ($errors->all() as $error)
+          // Display an info toast with no title
+            toastr.error("{{ $error }}")
+          @endforeach
+
+        @endif
+    </script>
 </body>
 
 </html>
