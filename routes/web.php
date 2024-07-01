9d62f2519771b8b353a9ab48744c5a90f1c297fd
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2024-07-01 07:32:17 +0600

    admin login page added

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
+
+use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 
 use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/** Admin Auth Routes */
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('admin/login', [AdminAuthController::class, 'index'])->name('admin.login');
+});
 
 Route::get('/',[FrontendController::class,'index'])->name('home');
 
