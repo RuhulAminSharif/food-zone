d4ed36c3655a8dfcf50a7560ba5245297d30f825
JNoor <108423527+Noorjannat@users.noreply.github.com>
2024-09-05 23:12:18 +0600

    general setting issue

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
+use App\Models\Setting;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -13,8 +13,25 @@ class SettingController extends Controller
 
     }
     function UpdateGeneralSetting(Request $request) {
-        dd($request->all());
+    $validatedData = $request->validate([
+        'site_name' => ['required','max:255'],
+      // 'site_email' => ['nullable', 'max:255'],
+       // 'site_phone' => ['nullable', 'max:255'],
+        'site_default_currency' => ['required', 'max:4'],
+        'site_currency_icon' => ['required', 'max:4'],
+        'site_currency_icon_position' => ['required', 'max:255'],
+       ]);
 
+       foreach ($validatedData as $key => $value) {
+        Setting::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
     }
+
+    toastr()->success('Updated Successfully!');
+
+    return redirect()->back();
+ }
 
 }
