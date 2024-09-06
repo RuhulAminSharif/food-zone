957642173186afc2749e7425d32a22e72fb4fb97
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2024-09-06 20:22:49 +0600

    added add to cart on product pop up

---
+++ app/Http/Controllers/Frontend/CartController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Cart;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+
+    // function index() : View {
+    //     return view('frontend.pages.cart-view');
+    // }
+
+    /**
+     *  Add product in to cart
+     */
+    function addToCart(Request $request)
+    {
+        $product = Product::with(['productSizes', 'productOptions'])->findOrFail($request->product_id);
+        // if($product->quantity < $request->quantity){
+        //     throw ValidationException::withMessages(['Quantity is not available!']);
+        // }
+
+        try {
+            $productSize = $product->productSizes->where('id', $request->product_size)->first();
+            $productOptions = $product->productOptions->whereIn('id', $request->product_option);
+
+            $options = [
+                'product_size' => [],
+                'product_options' => [],
+                'product_info' => [
+                    'image' => $product->thumb_image,
+                    'slug' => $product->slug
+                ]
+            ];
+
+            if ($productSize !== null) {
+                $options['product_size'] = [
+                    'id' => $productSize?->id,
+                    'name' => $productSize?->name,
+                    'price' => $productSize?->price
+                ];
+            }
+
+            foreach ($productOptions as $option) {
+                $options['product_options'][] = [
+                    'id' => $option->id,
+                    'name' => $option->name,
+                    'price' => $option->price
+                ];
+            }
+
+            Cart::add([
+                'id' => $product->id,
+                'name' => $product->name,
+                'qty' => $request->quantity,
+                'price' => $product->offer_price > 0 ? $product->offer_price : $product->price,
+                'weight' => 0,
+                'options' => $options
+            ]);
+
+            return response(['status' => 'success', 'message' => 'Product added into cart!'], 200);
+        } catch (\Exception $e) {
+            logger($e);
+            return response(['status' => 'error', 'message' => 'Something went wrong!'], 500);
+        }
+    }
+
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Frontend\CartController;
 use App\Http\Controllers\Frontend\DashboardController;
 use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\Frontend\ProfileController;
@@ -32,3 +33,8 @@ Route::get('/product/{slug}', [FrontendController::class, 'showProduct'])->name(
 
 /** Product Modal Route */
 Route::get('/load-product-modal/{productId}', [FrontendController::class, 'loadProductModal'])->name('load-product-modal');
+
+/** Add to cart Route */
+Route::post('add-to-cart', [CartController::class, 'addToCart'])->name('add-to-cart');
+// Route::get('get-cart-products', [CartController::class, 'getCartProduct'])->name('get-cart-products');
+// Route::get('cart-product-remove/{rowId}', [CartController::class, 'cartProductRemove'])->name('cart-product-remove');
