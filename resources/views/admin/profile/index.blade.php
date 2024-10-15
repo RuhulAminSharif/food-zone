eade0bd5ab27e890cf055bc3ea7b3d301c009284
RuhulAminSharif <sharif.cse.ras@gmail.com>
2024-10-15 15:19:52 +0600

    admin dashboard update profile heading changed

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -9,7 +9,7 @@
         <div class="section-body">
             <div class="card card-primary">
                 <div class="card-header">
-                    <h4>Update User Settings</h4>
+                    <h4>Update User Information</h4>
 
                 </div>
                 <div class="card-body">
